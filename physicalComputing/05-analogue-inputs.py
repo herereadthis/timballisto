a2a5c3ac27e79e9098ee6488f979df994127fa90
herereadthis <herereadthis@gmail.com>
2017-06-16 00:15:09 -0400

    mcp3008

--- physicalComputing/05-analogue-inputs.py
+++ physicalComputing/05-analogue-inputs.py
@@ -11,6 +11,25 @@ from gpiozero import MCP3008, PWMLED
 import math
 from time import sleep
 
+# How to wire an MCP3008 Analogue-to-Digital Converter (ADC)
+# The side with the notch is 0, the other side is 8.
+# if the notch is facing to your left, connect wires from the raspberry pi from
+# the side that is to the top of the notch (start from top left is pin 16, top
+# right is 09)
+#
+# pin 16 goes to 3V
+# pin 15 goes to 3V
+# pin 14 goes to GND (or pin #17)
+# pin 13 goes to SPI SCLK GP11
+# pin 12 goes to SPI MISO GP09
+# pin 11 goes to SPI MOSI GP10
+# pin 10 goes to SPI CE0 GP08
+# pin 09 goes to GND (or pin #20)
+
+# when connecting potentiometers, middle pin goes to analogue side of MCP3008
+# top or bottom go to 3V or GND. If you switch which goes to 3v and which goes
+# to GND, then the dial is reversed, no worries.
+
 pot1 = MCP3008(0)
 pot2 = MCP3008(1)
 
