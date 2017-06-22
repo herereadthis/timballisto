cd988ff7727fad6e07211b961bf09cc62b1be6bd
herereadthis <herereadthis@gmail.com>
2017-06-21 22:31:14 -0400

    this sensor sucks

--- physicalComputing/06-02-pir_led.py
+++ physicalComputing/06-02-pir_led.py
@@ -4,30 +4,57 @@
 import RPi.GPIO as GPIO
 import time
 
-# Define pin 3 as an output pin
-ledPin = 3
+# GPIO pin for light
+ledPin = 4
+# GPIO pin for sensor
+pirPin = 17
 
 # the chip - Broadcom Numbering System
 GPIO.setmode(GPIO.BCM)
 
+# no need to disble warnings for now
 # GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
+
 GPIO.setup(ledPin, GPIO.OUT)
+GPIO.setup(pirPin, GPIO.IN)
 
-try:
-    while True:
-        # Outputs digital HIGH signal (5V) on GPIO Pin 3
-        GPIO.output(ledPin, 1)
-        # Time delay of 1 second
-        time.sleep(1)
 
-        # Outputs digital LOW signal (0V) on GPIO Pin 3
-        GPIO.output(ledPin, 0)
-        # Time delay of 1 second
-        time.sleep(1)
+def led_test():
+    """Test to make sure the LED is working."""
+    try:
+        while True:
+            # Outputs digital HIGH signal (5V) on GPIO Pin 3
+            GPIO.output(ledPin, 1)
+            # Time delay
+            time.sleep(0.25)
 
-# this is how we quit the script
-except KeyboardInterrupt:
-    # reset all the GPIO to a safe place so we can use pi for something else
-    # clean up
-    GPIO.cleanup()
+            # Outputs digital LOW signal (0V) on GPIO Pin 3
+            GPIO.output(ledPin, 0)
+            # Time delay
+            time.sleep(0.25)
+
+    # this is how we quit the script
+    except KeyboardInterrupt:
+        # reset all the GPIO to a safe place so we can use pi for something
+        # else - clean up
+        GPIO.cleanup()
+
+
+def pir_led():
+    """trigger the led with the pir sensor."""
+    try:
+        while True:
+            i = GPIO.input(pirPin)
+
+            if i == 0:
+                print("nNo intruders",i)
+                GPIO.output(ledPin, 0)
+                time.sleep(0.1)
+            elif i == 1:
+                print("intruder detected",i)
+                GPIO.output(ledPin, 1)
+                time.sleep(0.1)
+                
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+
