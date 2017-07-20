1c767161f639e49735eae30b1c9806b695b86100
herereadthis <herereadthis@gmail.com>
2017-07-19 22:43:08 -0400

    class for accessing spreadsheet

--- google/README.md
+++ google/README.md
@@ -7,6 +7,18 @@ Connect a Raspberry Pi to Google services.
 ```bash
 # Super simple Google Spreadsheets Python API
 pip install gspread
+
+
+# Python library for accessing resources protected by OAuth 2.0
+# oauth2client is deprecated and the official recommendation is use google-auth
+# but google-auth is incompatible with gspread
+sudo pip install oauth2client
 ```
 
 ## Get Google OAuth2 Credentials
+
+* Go to the [Google Developers Console](https://console.developers.google.com/project) and make a new project.
+[//]: # * Then go to [Google Cloud Console Dashboad](https://console.cloud.google.com), select your new project, and click to "Enable API." Choose "Drive API" because we want to mess with Google Spreadsheets.
+* Find the link to "Credentials" to create a new "Service Account Key."
+* Then find the link to "Enable" and click on that
+* Create a new service account, probably "Project > Owner" and give it an account name. Choose "JSON" key type and create.
--- google/spreadsheet.py
+++ google/spreadsheet.py
@@ -1,22 +1,49 @@
-"""Update a spreadsheet."""
+"""Update a spreadsheet with gspread."""
 
 import gspread
-# import google.auth
+# Official recommendation - currently incompatible with gspread.
 # from google.oauth2 import service_account
 from oauth2client.service_account import ServiceAccountCredentials
-
-# from auth import (
-#     drive_api
-# )
-
-# credentials = service_account.Credentials.from_service_account_info(
-#     drive_api)
-
-# credentials = service_account.Credentials.from_service_account_file(
-#     'credentials.json')
-scope = ['https://spreadsheets.google.com/feeds']
-credentials = ServiceAccountCredentials.from_json_keyfile_name(
-    'credentials.json', scope
+from auth import (
+    drive_api
 )
+spreadsheet_id = '1qWSmRq0ttzX7h4h6pymw6ryzaiVIn27DgiiXaBz7bzM'
 
-gc = gspread.authorize(credentials)
+
+class Spreadsheet:
+    """Create a spreadsheet instance."""
+
+    def __init__(self, credentials):
+        """Initialize stuff."""
+        self.scope = scope = ['https://spreadsheets.google.com/feeds']
+        """
+        The official gspread documention recommends using:
+        ServiceAccountCredentials.from_json_keyfile_name() but that requires
+        saving the JSON file somewhere, which can easily be put into version
+        control by accident. Instead, save the JSON file somewhere else, then
+        use a separate auth.py file to load the data as a dictionary.
+        """
+        self.credentials = ServiceAccountCredentials.from_json_keyfile_dict(
+            credentials, scope
+        )
+        self.gc = gspread.authorize(self.credentials)
+
+    def set_spreadsheet(self, spreadsheet_id, worksheet_index=0):
+        """Pick a spreadsheet to update.
+
+        Remember to:
+        * Actually create the spreadsheet
+        * Share the spreadsheet with the email provided in credentials JSON
+        """
+        self.worksheet = self.gc.open_by_key(spreadsheet_id).get_worksheet(
+            worksheet_index
+        )
+
+    def get_value(self, cell):
+        """Get the value of a cell."""
+        return self.worksheet.acell(cell).value
+
+
+file_to_update = Spreadsheet(drive_api)
+file_to_update.set_spreadsheet(spreadsheet_id)
+print(file_to_update.get_value('A1'))
