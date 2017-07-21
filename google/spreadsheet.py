923586820a58f0854dec54a0ec302298adba93f7
herereadthis <herereadthis@gmail.com>
2017-07-20 23:05:07 -0400

    start time, end time, cuccessful)

--- google/spreadsheet.py
+++ google/spreadsheet.py
@@ -28,6 +28,7 @@ class Spreadsheet:
             credentials, scope
         )
         self.gc = gspread.authorize(self.credentials)
+        self.cell_pos = {}
 
     def set_worksheet(self, spreadsheet_id, worksheet_index=0):
         """Pick a spreadsheet to update.
@@ -44,14 +45,48 @@ class Spreadsheet:
         """Get the value of a cell."""
         return self.worksheet.acell(cell).value
 
+    def set_cell_position(self, row):
+        """Set coordinates of row to work with."""
+        self.cell_pos = {
+            'start_date': 'A%s' % (row),
+            'start_time': 'B%s' % (row),
+            'end_date': 'C%s' % (row),
+            'end_time': 'D%s' % (row),
+            'duration': 'E%s' % (row)
+        }
 
-spreadsheet = Spreadsheet(drive_api)
-spreadsheet.set_worksheet(spreadsheet_id)
+    def get_duration_formula(self):
+        """Make duration."""
+        c = self.cell_pos
+        return '=(%s - %s) + (%s - %s)' % (
+            c['end_date'], c['start_date'], c['end_time'], c['start_time']
+        )
+
+
 timestamp = datetime.utcnow()
 local_now = datetime.now()
-# print(datetime.utcnow())
+
 current_date = local_now.strftime('%Y-%m-%d')
 current_time = local_now.strftime('%H:%M:%S')
-print(local_now.strftime('%Y-%m-%d'))
-print(local_now.strftime('%H:%M:%S'))
-spreadsheet.worksheet.append_row([current_date, current_time])
+print('Time and date: %s %s' % (current_date, current_time))
+
+# Begin the spreadsheet instance.
+spreadsheet = Spreadsheet(drive_api)
+spreadsheet.set_worksheet(spreadsheet_id)
+row_count = spreadsheet.worksheet.row_count
+
+spreadsheet.set_cell_position(row_count)
+c = spreadsheet.cell_pos
+end_date_cell_value = spreadsheet.get_value(c['end_date'])
+
+# If no end timer entry is found, add to last row
+if end_date_cell_value == '':
+    print('End Timer')
+    duration = spreadsheet.get_duration_formula()
+    spreadsheet.worksheet.update_acell(c['end_date'], current_date)
+    spreadsheet.worksheet.update_acell(c['end_time'], current_time)
+    spreadsheet.worksheet.update_acell(c['duration'], duration)
+# Else, create new row with start timer entry
+else:
+    print('Start Timer')
+    spreadsheet.worksheet.append_row([current_date, current_time])
