d286e05eb65ef67168f4103025638dc6f259d5d9
herereadthis <herereadthis@gmail.com>
2017-07-22 00:14:48 -0400

    i am really sending a message, nice.

--- sms/auth.py
+++ sms/auth.py
@@ -7,3 +7,4 @@ with open('./../../secret.json') as json_data:
     account_sid = data.get('twilio', {}).get('accountSID', {})
     auth_token = data.get('twilio', {}).get('authToken', {})
     owner_phone = data.get('twilio', {}).get('ownerPhone', {})
+    sender_phone = data.get('twilio', {}).get('senderPhone', {})
--- sms/send_sms.py
+++ sms/send_sms.py
@@ -4,7 +4,15 @@ from twilio.rest import Client
 from auth import (
     account_sid,
     auth_token,
-    owner_phone
+    owner_phone,
+    sender_phone
 )
 
 client = Client(account_sid, auth_token)
+
+# to_number = '+%s' % owner_phone
+to_number = '+17023250770'
+from_number = '+%s' % sender_phone
+message = 'Hello this is test message from Jimmy'
+
+client.messages.create(to=to_number, from_=from_number, body=message)
