bdb8f02b42a827dd6392678e320f51d1ddc02d2e
herereadthis <herereadthis@gmail.com>
2017-07-24 21:37:00 -0400

    class definition

--- objectives/i2c_lcd/i2clcdb.py
+++ objectives/i2c_lcd/i2clcdb.py
@@ -32,84 +32,80 @@ E_DELAY = 0.0005
 
 # Open I2C interface
 # bus = smbus.SMBus(0)  # Rev 1 Pi uses 0
-bus = smbus.SMBus(1)  # Rev 2 Pi uses 1
+# bus = smbus.SMBus(1)  # Rev 2 Pi uses 1
 
 
-def lcd_init():
-    # Initialise display
-    lcd_byte(0x33, LCD_CMD)  # 110011 Initialise
-    lcd_byte(0x32, LCD_CMD)  # 110010 Initialise
-    lcd_byte(0x06, LCD_CMD)  # 000110 Cursor move direction
-    lcd_byte(0x0C, LCD_CMD)  # 001100 Display On,Cursor Off, Blink Off
-    lcd_byte(0x28, LCD_CMD)  # 101000 Data length, number of lines, font size
-    lcd_byte(0x01, LCD_CMD)  # 000001 Clear display
-    time.sleep(E_DELAY)
+class I2cLcd:
 
+    def __init__(self):
+        self.bus = smbus.SMBus(1)
 
-def lcd_byte(bits, mode):
-    # Send byte to data pins
-    # bits = the data
-    # mode = 1 for data
-    #        0 for command
+        # Initialise display
+        self.lcd_byte(0x33, LCD_CMD)  # 110011 Initialise
+        self.lcd_byte(0x32, LCD_CMD)  # 110010 Initialise
+        self.lcd_byte(0x06, LCD_CMD)  # 000110 Cursor move direction
+        self.lcd_byte(0x0C, LCD_CMD)  # 001100 Display On,Cursor Off, Blink Off
+        # 101000 Data length, number of lines, font size
+        self.lcd_byte(0x28, LCD_CMD)
+        self.lcd_byte(0x01, LCD_CMD)  # 000001 Clear display
+        time.sleep(E_DELAY)
 
-    bits_high = mode | (bits & 0xF0) | LCD_BACKLIGHT
-    bits_low = mode | ((bits << 4) & 0xF0) | LCD_BACKLIGHT
+    def lcd_byte(self, bits, mode):
+        bits_high = mode | (bits & 0xF0) | LCD_BACKLIGHT
+        bits_low = mode | ((bits << 4) & 0xF0) | LCD_BACKLIGHT
 
-    # High bits
-    bus.write_byte(I2C_ADDR, bits_high)
-    lcd_toggle_enable(bits_high)
+        # High bits
+        self.bus.write_byte(I2C_ADDR, bits_high)
+        self.lcd_toggle_enable(bits_high)
 
-    # Low bits
-    bus.write_byte(I2C_ADDR, bits_low)
-    lcd_toggle_enable(bits_low)
+        # Low bits
+        self.bus.write_byte(I2C_ADDR, bits_low)
+        self.lcd_toggle_enable(bits_low)
 
+    def lcd_toggle_enable(self, bits):
+        # Toggle enable
+        time.sleep(E_DELAY)
+        self.bus.write_byte(I2C_ADDR, (bits | ENABLE))
+        time.sleep(E_PULSE)
+        self.bus.write_byte(I2C_ADDR, (bits & ~ENABLE))
+        time.sleep(E_DELAY)
 
-def lcd_toggle_enable(bits):
-    # Toggle enable
-    time.sleep(E_DELAY)
-    bus.write_byte(I2C_ADDR, (bits | ENABLE))
-    time.sleep(E_PULSE)
-    bus.write_byte(I2C_ADDR, (bits & ~ENABLE))
-    time.sleep(E_DELAY)
+    def lcd_string(self, message, line):
+        # Send string to display
 
+        message = message.ljust(LCD_WIDTH, " ")
 
-def lcd_string(message, line):
-    # Send string to display
+        self.lcd_byte(line, LCD_CMD)
 
-    message = message.ljust(LCD_WIDTH, " ")
-
-    lcd_byte(line, LCD_CMD)
-
-    for i in range(LCD_WIDTH):
-        lcd_byte(ord(message[i]), LCD_CHR)
+        for i in range(LCD_WIDTH):
+            self.lcd_byte(ord(message[i]), LCD_CHR)
 
 
 def main():
     # Main program block
 
     # Initialise display
-    lcd_init()
-
-    while True:
-
-        # Send some test
-        lcd_string("Hello World!", LCD_LINE_1)
-        lcd_string("Can you read me?", LCD_LINE_2)
-
-        time.sleep(3)
-
-        # Send some more text
-        lcd_string("This is dope.", LCD_LINE_1)
-        lcd_string("But too procedural.", LCD_LINE_2)
-
-        time.sleep(3)
-
-
-if __name__ == '__main__':
+    # lcd_init()
+    i2c_lcd = I2cLcd()
 
     try:
-        main()
+        while True:
+            # Send some test
+            i2c_lcd.lcd_string("77777", LCD_LINE_1)
+            i2c_lcd.lcd_string("la la la", LCD_LINE_2)
+
+            time.sleep(3)
+
+            # Send some more text
+            i2c_lcd.lcd_string("What are you", LCD_LINE_1)
+            i2c_lcd.lcd_string("doing???", LCD_LINE_2)
+
+            time.sleep(3)
     except KeyboardInterrupt:
         pass
     finally:
-        lcd_byte(0x01, LCD_CMD)
+        i2c_lcd.lcd_byte(0x01, LCD_CMD)
+
+
+if __name__ == '__main__':
+    main()
