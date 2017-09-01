5f989e9090b7bf48eaf0c9bdf47b9532047553d9
herereadthis <herereadthis@gmail.com>
2017-08-31 23:31:59 -0400

    bunch processing

--- tutorial_notes/rpi_org_projects/mandelbrot.py
+++ tutorial_notes/rpi_org_projects/mandelbrot.py
@@ -1,7 +1,10 @@
 """Graph the mandelbrot set."""
+# https://www.raspberrypi.org/magpi-issues/MagPi57.pdf
+# https://github.com/jhobro/magpi_parallel2
 
 from numpy import linspace, reshape
 from matplotlib import pyplot
+from multiprocessing import Pool
 
 # x range
 xmin = -2.0
@@ -15,6 +18,11 @@ ny = 1000
 # max iterations
 maxiter = 50
 
+# lists of x and y
+X = linspace(xmin, xmax, nx)
+# pixel co-ordinates
+Y = linspace(ymin, ymax, ny)
+
 
 def mandelbrot(z):
     """Compute for each pixel."""
@@ -24,12 +32,6 @@ def mandelbrot(z):
             return n
         z = z * z + c
     return maxiter
-
-
-# lists of x and y
-X = linspace(xmin, xmax, nx)
-# pixel co-ordinates
-Y = linspace(ymin, ymax, ny)
 
 
 def reshape_array(values_array):
@@ -57,13 +59,42 @@ def map_loop():
     return loop_list
 
 
+def pool_processing():
+    """Create multi-process pool."""
+    p = Pool()
+    pre_map = [complex(x, y) for y in Y for x in X]
+    loop_list = p.map(mandelbrot, pre_map)
+    loop_list = reshape_array(loop_list)
+    return loop_list
+
+
+def compute_all_x(y):
+    """Define inner loop."""
+    pre_map = [complex(x, y) for x in X]
+    return map(mandelbrot, pre_map)
+
+
+def bunch_processing():
+    """Bunch the multi-process."""
+    p = Pool()
+    loop_list = p.map(compute_all_x, Y)
+    loop_list = reshape_array(loop_list)
+    return loop_list
+
+
 if __name__ == '__main__':
 
     # Simple loop version
     # N = simple_loop()
 
     # Map version
-    N = map_loop()
+    # N = map_loop()
+
+    # Parallel
+    # N = pool_processing()
+
+    # bunch
+    N = bunch_processing()
 
     # plot the image
     pyplot.imshow(N)
