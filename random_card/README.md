704d0f0d507438a168197bf70271b0900fde8f1b
herereadthis <herereadthis@gmail.com>
2017-09-05 23:27:25 -0400

    random card

---
+++ random_card/README.md
@@ -0,0 +1,8 @@
+# Random card from the street
+
+Imagine if once a day, you picked up a random card off the street. What is the expected number of days it will take before you have a complete deck?
+
+Without getting into too much detail, this question is a variation of the coupon collector's problem and is related to harmonic numbers: n * H<sub>n</sub>, or `52 * H_52 = 236`. [Here is the solution](http://www.wolframalpha.com/input/?i=52+*+H_52)
+
+Run a simulation to see if the average number from X trials will match 236.
+
---
+++ random_card/random_card.py
@@ -0,0 +1,83 @@
+"""Draw cards randomly from a self-replacing pile until you have a deck."""
+
+import random
+import collections
+import matplotlib.pyplot as plt
+import math
+
+cards = 52
+deck = []
+turns = 0
+trials = []
+iterations = input('How many simulations [10]?') or 10
+iterations = int(iterations)
+
+def get_counter(trials_list):
+    """Use collections."""
+    counter = collections.Counter(trials_list)
+    return counter
+
+
+def get_average(num_list):
+    """Get average of a list."""
+    return sum(num_list) / len(num_list)
+
+
+def get_most_common_frequency(trials_list):
+    """Get the frequency of most commonly occuring result."""
+    counter = get_counter(trials_list)
+    common = list(counter.most_common(1))
+    most_common = list(dict(common).values())[0]
+
+    return most_common
+
+
+def get_most_common_result(trials_list):
+    """Get the most commonly occuring result."""
+    counter = get_counter(trials_list)
+    common = list(counter.most_common(1))
+    most_common = list(dict(common).keys())[0]
+
+    return most_common
+
+
+for k in range(iterations):
+    while len(deck) < cards:
+        a = random.randint(1, cards)
+        if a not in deck:
+            deck.append(a)
+        turns += 1
+
+    trials.append(turns)
+    turns = 0
+    deck = []
+    k += 1
+
+print('Trials: %s' % (trials))
+print('Sorted: %s' % (sorted(trials)))
+print('Average: %s' % (get_average(trials)))
+
+
+
+sorted_trials = sorted(trials)
+
+counter = collections.Counter(sorted_trials)
+
+values = list(counter.values())
+keys = list(counter.keys())
+
+most_common_frequency = get_most_common_frequency(trials)
+most_common_result = get_most_common_result(trials)
+print(most_common_frequency)
+print(most_common_result)
+
+print(math.ceil(most_common_frequency / 5))
+
+y_max = math.ceil(most_common_frequency / 5) * 5
+x_max = max(trials)
+
+plt.plot(keys, values, 'ro')
+plt.axis([0, x_max, 0, y_max])
+plt.show()
+
+
