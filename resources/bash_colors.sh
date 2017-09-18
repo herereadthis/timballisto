02b14a9d9bed7cc81a15ba54ee3bb1f57fbcd1a8
herereadthis <herereadthis@gmail.com>
2017-09-17 22:41:52 -0400

    color refactor

--- resources/bash_colors.sh
+++ resources/bash_colors.sh
@@ -22,51 +22,51 @@ B_CYAN='\033[1;36m'        # Cyan
 B_WHITE='\033[1;37m'       # White
 
 # Underline
-UBLACK='\033[4;30m'       # Black
-URED='\033[4;31m'         # Red
-UGREEN='\033[4;32m'       # Green
-UYELLOW='\033[4;33m'      # Yellow
-UBLUE='\033[4;34m'        # Blue
-UPURPLE='\033[4;35m'      # Purple
-UCYAN='\033[4;36m'        # Cyan
-UWHITE='\033[4;37m'       # White
+U_BLACK='\033[4;30m'       # Black
+U_RED='\033[4;31m'         # Red
+U_GREEN='\033[4;32m'       # Green
+U_YELLOW='\033[4;33m'      # Yellow
+U_BLUE='\033[4;34m'        # Blue
+U_PURPLE='\033[4;35m'      # Purple
+U_CYAN='\033[4;36m'        # Cyan
+U_WHITE='\033[4;37m'       # White
 
 # Background
-On_BLACK='\033[40m'       # Black
-On_RED='\033[41m'         # Red
-On_GREEN='\033[42m'       # Green
-On_YELLOW='\033[43m'      # Yellow
-On_BLUE='\033[44m'        # Blue
-On_PURPLE='\033[45m'      # Purple
-On_CYAN='\033[46m'        # Cyan
-On_WHITE='\033[47m'       # White
+BG_BLACK='\033[40m'       # Black
+BG_RED='\033[41m'         # Red
+BG_GREEN='\033[42m'       # Green
+BG_YELLOW='\033[43m'      # Yellow
+BG_BLUE='\033[44m'        # Blue
+BG_PURPLE='\033[45m'      # Purple
+BG_CYAN='\033[46m'        # Cyan
+BG_WHITE='\033[47m'       # White
 
 # High Intensty
-IBLACK='\033[0;90m'       # Black
-IRED='\033[0;91m'         # Red
-IGREEN='\033[0;92m'       # Green
-IYELLOW='\033[0;93m'      # Yellow
-IBLUE='\033[0;94m'        # Blue
-IPURPLE='\033[0;95m'      # Purple
-ICYAN='\033[0;96m'        # Cyan
-IWHITE='\033[0;97m'       # White
+I_BLACK='\033[0;90m'       # Black
+I_RED='\033[0;91m'         # Red
+I_GREEN='\033[0;92m'       # Green
+I_YELLOW='\033[0;93m'      # Yellow
+I_BLUE='\033[0;94m'        # Blue
+I_PURPLE='\033[0;95m'      # Purple
+I_CYAN='\033[0;96m'        # Cyan
+I_WHITE='\033[0;97m'       # White
 
 # Bold High Intensty
-BIBLACK='\033[1;90m'      # Black
-BIRED='\033[1;91m'        # Red
-BIGREEN='\033[1;92m'      # Green
-BIYELLOW='\033[1;93m'     # Yellow
-BIBLUE='\033[1;94m'       # Blue
-BIPURPLE='\033[1;95m'     # Purple
-BICYAN='\033[1;96m'       # Cyan
-BIWHITE='\033[1;97m'      # White
+BI_BLACK='\033[1;90m'      # Black
+BI_RED='\033[1;91m'        # Red
+BI_GREEN='\033[1;92m'      # Green
+BI_YELLOW='\033[1;93m'     # Yellow
+BI_BLUE='\033[1;94m'       # Blue
+BI_PURPLE='\033[1;95m'     # Purple
+BI_CYAN='\033[1;96m'       # Cyan
+BI_WHITE='\033[1;97m'      # White
 
 # High Intensty backgrounds
-On_IBLACK='\033[0;100m'   # Black
-On_IRED='\033[0;101m'     # Red
-On_IGREEN='\033[0;102m'   # Green
-On_IYELLOW='\033[0;103m'  # Yellow
-On_IBLUE='\033[0;104m'    # Blue
-On_IPURPLE='\033[10;95m'  # Purple
-On_ICYAN='\033[0;106m'    # Cyan
-On_IWHITE='\033[0;107m'   # White
+IBG_BLACK='\033[0;100m'   # Black
+IBG_RED='\033[0;101m'     # Red
+IBG_GREEN='\033[0;102m'   # Green
+IBG_YELLOW='\033[0;103m'  # Yellow
+IBG_BLUE='\033[0;104m'    # Blue
+IBG_PURPLE='\033[10;95m'  # Purple
+IBG_CYAN='\033[0;106m'    # Cyan
+IBG_WHITE='\033[0;107m'   # White
