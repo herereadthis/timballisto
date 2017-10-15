86d39a53b4822fbb5957ce7498c3b99ffb161a51
herereadthis <herereadthis@gmail.com>
2017-10-14 23:56:44 -0400

    inheritance

---
+++ composition/README.md
@@ -0,0 +1,11 @@
+# Composition
+
+
+Object B is a specialization of object A when:
+
+* B has all the features of A
+* B has some other features too
+* B can do some or all the things A can do, but differently
+
+
+Delegation is an object that does only what it knows best, and leave everything else to other objects.
--- composition/door_03.py
+++ composition/door_03.py
@@ -25,14 +25,30 @@ class Door:
 class SecurityDoor(Door):
     """Inherit from Door class."""
 
-    colour = 'gray'
+    # Overrides Door.color
+    color = 'gray'
     locked = True
 
     def open(self):
         """Open door."""
+        # This will override Door.open()
         if not self.locked:
             self.status = 'open'
 
 
 door1 = Door(1, 'closed')
-door2 = Door(2, 'closed')
+door2 = SecurityDoor(2, 'closed')
+
+print(door1.__dict__)
+# >>> {'number': 1, 'status': 'closed'}
+print(door2.__dict__)
+# >>> {'number': 2, 'status': 'closed'}
+print(id(door1.color) == id(Door.color))
+# >>> True
+print(id(door2.color) == id(SecurityDoor.color))
+# >>> True
+
+
+
+print(id(SecurityDoor.color))
+print(id(Door.color))
