06bb23b7185267bc47f3f4e3187ae5eed6d09667
herereadthis <herereadthis@gmail.com>
2017-10-27 22:20:02 -0400

    actual unit testing

--- decorators/static.py
+++ decorators/static.py
@@ -3,18 +3,21 @@
 """
 A staticmethod is a method that knows nothing about the class or the instance
 it was called on. It just gets the arguments that were passed, with no implicit
-first argument. You would want to use it because it logically belongs in a 
+first argument. You would want to use it because it logically belongs in a
 class but it doesn't require access to the class.
 """
+
 
 class MyClass:
     """Demo staticmethod decorator."""
 
     def foo(self, x):
+        """Demo instance method."""
         print('execute foo(%s%s) method' % (self, x))
 
     @staticmethod
     def static_foo(x):
+        """Demo static method."""
         print('execute foo(%s) method' % (x))
 
 
