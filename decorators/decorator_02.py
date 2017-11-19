156c0eaf6aa28898e49a0af1b2e7b900e760214e
herereadthis <herereadthis@gmail.com>
2017-11-19 00:05:14 -0500

    more decorators

--- decorators/decorator_02.py
+++ decorators/decorator_02.py
@@ -1,6 +1,17 @@
+"""Explore decorators."""
+
+
+"""
+The output of a function is a reference to an object.
+Therefore, functions can return references to functions, because everything is
+an object.
+"""
+
+
 def f(x):
+    """Return a function."""
     def g(y):
-        return y + x + 3 
+        return y + x + 3
     return g
 
 nf1 = f(1)
@@ -8,3 +19,36 @@ nf2 = f(3)
 
 print(nf1(1))
 print(nf2(1))
+
+
+def my_decorator(func):
+    """Create a simple decorator."""
+    def function_wrapper(x):
+        print('Before calling {}'.format(func.__name__))
+        """The function is the parameter."""
+        func(x)
+        print('After calling {}'.format(func.__name__))
+    return function_wrapper
+
+
+def foo(x):
+    print("Hi, foo has been called with " + str(x))
+
+@my_decorator
+def bar(x):
+    print("Hi, bar has been called with " + str(x))
+
+print("\n***Call foo before decoration:\n")
+foo("Hi")
+
+print("\n***Decorate foo with f:\n")
+# This is not a good design pattern. Now there are two versions of foo, one
+# before and one after decoration.
+foo = my_decorator(foo)
+
+print("\n***Call foo after decoration:\n")
+foo(42)
+
+
+print("\n***Call decorated bar:\n")
+bar(73)
